a8b568aae159eae93309abdd4fba2acaa0bd039b
Jackson Horan <JacksonHoran1@gmail.com>
2026-06-04 14:31:25 -0400

    fix url path

---
+++ frontend/src/router/index.js
@@ -0,0 +1,57 @@
+import { createRouter, createWebHistory } from "vue-router";
+import ArticleForm from "@/components/ArticleForm.vue";
+import Articles from "@/views/Articles.vue";
+import Login from "@/views/Login.vue";
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+        path: "/login",
+        name: "login",
+        component: Login,
+    },
+    {
+        path: "/logout",
+        name: "logout",
+        component: Login,
+    },
+    {
+      path: "/articles",
+      name: "articles-list",
+      component: Articles,
+    },
+    {
+      path: "/articles/:id",
+      name: "article-details",
+      component: Articles,
+    },
+    {
+      path: "/articles/add",
+      name: "add-article",
+      component: ArticleForm,
+    },
+    {
+      path: "/articles/edit/:id",
+      name: "edit-article",
+      component: ArticleForm,
+    },
+    {
+        path: '/articles/delete/:id',
+        name: 'delete-article',
+        component: Articles,
+    },
+
+  ],
+});
+
+export default router;
+
+// POST   /api/login
+// POST   /api/logout
+// GET    /api/articles
+// GET    /api/articles/:id
+// POST   /api/articles
+// PATCH  /api/articles/:id
+// DELETE /api/articles/:id

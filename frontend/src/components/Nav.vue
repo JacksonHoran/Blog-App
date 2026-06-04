59f7319902d77cd5d03400a6ded749ace7e89db8
Jackson Horan <JacksonHoran1@gmail.com>
2026-06-04 10:52:10 -0400

    add nav component

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <nav class="flex max-w-200 mx-auto mb-5 px-1 py-4 items-center justify-between">
+    <div>
+      <a class="text-3xl font-semibold cursor-pointer">Full Stack Blog</a>
+    </div>
+    <div>
+      <a class="px-2 text-xl cursor-pointer">Articles</a>
+      <a class="px-2 text-xl cursor-pointer">Log out</a>
+    </div>
+  </nav>
+</template>

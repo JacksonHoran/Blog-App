7faf150a7b8cc971a02102b0668fd31515d5ad9f
Jackson Horan <JacksonHoran1@gmail.com>
2026-06-04 14:15:52 -0400

    implement add logic

--- frontend/src/components/ArticleForm.vue
+++ frontend/src/components/ArticleForm.vue
@@ -1,15 +1,45 @@
 <script setup>
-import SubmitButton from './SubmitButton.vue';
+import { ref, watch } from "vue";
+import SubmitButton from "./SubmitButton.vue";
+
+const props = defineProps({
+  initialTitle: { type: String, default: "" },
+  initalBody: { type: String, default: "" },
+  buttonText: { type: String, default: "Save" },
+  errorMessage: { type: String, default: "" },
+});
+
+const emit = defineEmits(["submitForm"]);
+
+const title = ref(props.initialTitle);
+const body = ref(props.initalBody);
+
+watch(
+  () => props.initialTitle,
+  (newVal) => (title.value = newVal),
+);
+watch(
+  () => props.initalBody,
+  (newVal) => (body.value = newVal),
+);
+
+const handleSubmit = () => {
+  emit("submitForm", {
+    title: title.value,
+    body: body.value,
+  });
+};
 </script>
 
 <template>
   <div class="bg-slate-100 max-w-200 mx-auto shadow-xl">
     <div class="p-5">
       <h2 class="text-3xl font-light">{{ pageTitle }}</h2>
-      <form>
+      <form @submit.prevent="handleSubmit">
         <div class="mt-2">
           <label class="font-semibold">Title</label>
           <input
+            v-model="title"
             class="my-2 border w-full rounded-md px-3 py-1"
             type="text"
             required />
@@ -17,11 +47,12 @@ import SubmitButton from './SubmitButton.vue';
         <div class="mt-2">
           <label class="font-semibold">Body</label>
           <textarea
+            v-model="body"
             class="my-2 border w-full rounded-md px-3 py-1"
             rows="5"
             required></textarea>
         </div>
-        <SubmitButton />
+        <SubmitButton type="submit" text="Save Article" />
       </form>
     </div>
   </div>
--- frontend/src/components/SubmitButton.vue
+++ frontend/src/components/SubmitButton.vue
@@ -1,8 +1,20 @@
-<script setup></script>
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: "submit",
+  },
+  text: {
+    type: String,
+    default: "Submit",
+  },
+});
+</script>
 
 <template>
   <button
+    :type="type"
     class="mt-3 py-1 px-3 bg-blue-300 rounded-xl hover:bg-blue-400 shadow-xl cursor-pointer">
-    Submit
+    {{ text }}
   </button>
 </template>

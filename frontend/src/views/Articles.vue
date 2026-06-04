cc1b90c5ac49d55bb2bdb5ae7c4b6135b6c51da4
Jackson Horan <JacksonHoran1@gmail.com>
2026-06-04 12:06:33 -0400

    Add Articles page

---
+++ frontend/src/views/Articles.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Nav from "../components/Nav.vue";
+</script>
+
+<template>
+  <Nav />
+  <div class="bg-slate-100 max-w-200 mx-auto shadow-xl">
+    <div class="p-5">
+      <h2 class="text-3xl font-light mb-5">Articles</h2>
+      <a
+        class="py-1 px-3 bg-blue-300 rounded-xl hover:bg-blue-400 shadow-xl cursor-pointer">
+        Add Article
+      </a>
+      <table class="table-auto w-full mt-8">
+        <thead class="text-left">
+            <th>Title</th>
+            <th>Author</th>
+            <th>Created</th>
+            <th>Action</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="py-3">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td>Jackson Horan</td>
+                <td>6/4/26</td>
+                <td>Edit  Delete</td>
+            </tr>
+            <tr>
+                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td>Jackson Horan</td>
+                <td>6/4/26</td>
+                <td><a>Edit</a>  <a>Delete</a></td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>

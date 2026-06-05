1d94aa83aa9f95fda7496ac7adbe78d3511f723b
Jackson Horan <JacksonHoran1@gmail.com>
2026-06-05 10:06:49 -0400

    implement hanndleLogin

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,9 +1,34 @@
 <script setup>
 import Nav from "@/components/Nav.vue";
+import api from "@/services/api";
 import SubmitButton from "@/components/SubmitButton.vue";
 import { useRoute } from "vue-router";
+import { ref } from "vue";
+import router from "@/router";
 
 const route = useRoute();
+
+const email = ref("");
+const password = ref("");
+const errorMessage = ref("");
+
+const handleLogin = async () => {
+  errorMessage.value = "";
+  try {
+    const response = await api.post("/users/login.json", {
+      email: email.value,
+      password: password.value,
+    });
+    console.log("Logged in: ", response.data);
+    router.push("/articles");
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      errorMessage.value = error.response.data.message;
+    } else {
+      errorMessage.value = "A server error occurred. Please try again later.";
+    }
+  }
+};
 </script>
 
 <template>
@@ -11,31 +36,39 @@ const route = useRoute();
   <div class="bg-slate-100 max-w-200 mx-auto shadow-xl">
     <div class="p-5">
       <h2 class="text-3xl font-light">Login</h2>
-      <form>
+      <form @submit.prevent="handleLogin">
         <fieldset class="mt-5">
           <legend class="font-semibold">
             Please enter your email and password
           </legend>
           <div class="mt-2">
             <label class="font-semibold">Email</label>
-            <input class="my-2 border w-full rounded-md px-3 py-1" />
+            <input
+              v-model="email"
+              type="email"
+              required
+              class="my-2 border w-full rounded-md px-3 py-1" />
           </div>
           <div class="mt-2">
             <label class="font-semibold">Password</label>
             <input
+              v-model="password"
+              required
               class="my-2 border w-full rounded-md px-3 py-1"
               type="password" />
           </div>
         </fieldset>
         <div class="flex gap-4">
           <SubmitButton />
-          <a
+          <router-link
+            to="/add-user"
             class="mt-3 py-1 px-3 bg-blue-300 rounded-xl hover:bg-blue-400 shadow-xl cursor-pointer"
-            >Add User</a
-          >
+            >Add User</router-link>
         </div>
       </form>
-      <p v-if="errorMessage">{{ errorMessage }}</p>
+      <p v-if="errorMessage" class="mt-4 text-red-500 font-semibold">
+        {{ errorMessage }}
+      </p>
     </div>
   </div>
 </template>

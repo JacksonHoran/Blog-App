8d715927331a94c9a747bb09155e214e4d61406a
Jackson Horan <JacksonHoran1@gmail.com>
2026-06-05 10:43:43 -0400

    add router links to nav buttons

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -3,11 +3,12 @@
 <template>
   <nav class="flex max-w-200 mx-auto mb-5 px-1 py-4 items-center justify-between">
     <div>
-      <a class="text-3xl font-semibold cursor-pointer">Full Stack Blog</a>
+      <router-link to="/articles" class="text-3xl font-semibold cursor-pointer">Full Stack Blog</router-link>
+
     </div>
     <div>
-      <a class="px-2 text-xl cursor-pointer">Articles</a>
-      <a class="px-2 text-xl cursor-pointer">Log out</a>
+      <router-link to="/articles" class="px-2 text-xl cursor-pointer">Articles</router-link>
+      <router-link to="/logout" class="px-2 text-xl cursor-pointer">Log Out</router-link>
     </div>
   </nav>
 </template>

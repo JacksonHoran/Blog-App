e81ad13c14c27cfcc0e3ef1dea1d90041542d517
Jackson Horan <JacksonHoran1@gmail.com>
2026-06-05 10:06:24 -0400

    reconfigure proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -17,11 +16,12 @@ export default defineConfig({
     },
   },
   server: {
+    port: 5174,
     proxy: {
       '/api': {
         target: 'http://localhost:8765',
         changeOrigin: true,
-        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '') 
       }
     }
   }

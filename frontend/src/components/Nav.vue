f4dcc5663ff2b0ba45d17f77ec716ce67bbe1f60
Jackson Horan <JacksonHoran1@gmail.com>
2026-06-08 15:26:44 -0400

    fix cookie destroy bug

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,29 +1,40 @@
 <script setup>
-import { useRouter } from 'vue-router';
+import { useAuth } from '@/composables/useAuth';
 import api from '@/services/api';
+const { isLoggedIn, logout } = useAuth();
 
+import { useRouter } from 'vue-router';
 const router = useRouter();
 
 const handleLogout = async () => {
   try {
-    await api.post('/users/logout.json');
+    await api.post('/users/logout.json'); 
+    logout();
     router.push('/login');
   } catch (error) {
-    console.error('Logout failed:', error);
+    console.error("Failed to destroy session on server", error);
   }
-}
+};
 </script>
 
 <template>
-  <nav class="flex max-w-200 mx-auto mb-5 px-1 py-4 items-center justify-between">
-    <div>
-      <router-link to="/articles" class="text-3xl font-semibold cursor-pointer">Full Stack Blog</router-link>
-
-    </div>
-    <div class="items-center">
+  <nav class="flex max-w-200 mx-auto mb-4 px-1 py-4 items-center justify-between">
+    <div class="text-3xl font-semibold cursor-pointer">My Blog</div>
+    
+    <div class="space-x-4 flex items-center">
       <router-link to="/articles" class="px-2 text-xl cursor-pointer text-blue-500 hover:text-blue-700 transition-colors font-medium">Articles</router-link>
-      <button @click="handleLogout" class="px-2 text-xl cursor-pointer text-blue-500 hover:text-blue-700 transition-colors font-medium">Logout</button>
+      
+      <template v-if="!isLoggedIn">
+        <router-link to="/login" class="px-2 text-xl cursor-pointer text-blue-500 hover:text-blue-700 transition-colors font-medium">Login</router-link>
+      </template>
+
+      <template v-else>
+        <button 
+          @click="handleLogout" 
+          class="px-2 text-xl cursor-pointer text-blue-500 hover:text-blue-700 transition-colors font-medium">
+          Logout
+        </button>
+      </template>
     </div>
   </nav>
-</template>
-          
+</template> 
